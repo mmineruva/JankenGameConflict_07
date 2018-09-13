b91ec78551bea6ac7976f63d432d79e7efc96143
kanta kikuchi <mmineruva1298@gmail.com>
2018-09-13 14:58:32 +0900

    あああああ

--- src/janken/JankenController.java
+++ src/janken/JankenController.java
@@ -16,6 +16,9 @@ public class JankenController {
       private Label myHandLabel;
       private Label resultLabel;
       private Label scoreLabel;
+      private int score = 0;
+
+
       //コンストラクタ
       //引数  3つのラベル
       public JankenController(Label cpuHandLabel, Label myHandLabel,
@@ -55,6 +58,14 @@ public class JankenController {
      private String getResult(int myHand, int cpuHand) {
          int r = (myHand - cpuHand + 3) % 3;
 
+         if(r == 2) {
+             score +=2;
+            } else if(r==1) {
+             score -=1;
+            } else {
+             score +=1;
+            }
+
          return (r == 2) ? "あなたの勝ち！" : ((r == 1) ? "あなたの負け！" : "あいこ！");
      }
      //自分の手・相手の手・じゃんけんの結果を各ラベルに表示
@@ -62,5 +73,7 @@ public class JankenController {
          cpuHandLabel.setText("コンピュータの手: " + hands[cpuHand]);
          myHandLabel.setText("あなたの手: " + hands[myHand]);
          resultLabel.setText("結果: " + getResult(myHand, cpuHand));
+         scoreLabel.setText(" Score:"+score);
      }
+
 }
